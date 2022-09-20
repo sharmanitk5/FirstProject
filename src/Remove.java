b264d19289670c25a6c117a67b0419c948d3dea4
sachin <sharmanitk5@gmail.com>
2022-09-20 12:41:33 -0400

    remove duplicate from sorted array

---
+++ src/Remove.java
@@ -0,0 +1,45 @@
+public class Remove {
+    public static void main(String[] args) {
+
+
+        int a[] = {1, 1, 2};
+        System.out.println(Increasing(a));
+    }
+
+    public static int Increasing(int[] a) {
+        //int count = 1;
+        int count = 1;
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i] != a[i - 1]) {
+                a[count++] = a[i];
+
+            }
+        }
+            return count;
+
+
+
+    }
+
+}
+
+
+
+   // }
+  //  public static boolean Incr(int a[]){
+   //     for(int i =0;i<a.length;i++) {
+    //        for (int j = 1; j < a.length; j++) {
+
+       //         if (a[i] < a[j]) {
+
+       //         }
+       //     }
+    //    }
+   //     return true;
+   // }
+
+
+
+
+  //  }
