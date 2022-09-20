ed1c1b5378af119039fea01ef00643951c9446ab
sachin <sharmanitk5@gmail.com>
2022-09-20 18:48:45 -0400

    new inputs being added

--- src/Remove.java
+++ src/Remove.java
@@ -2,7 +2,7 @@ public class Remove {
     public static void main(String[] args) {
 
 
-        int a[] = {1, 1, 2,2,3,4,5,6,7,7,8,9};
+        int a[] = {1, 1, 2,2,3,4,5,6,7,7,8,9,10,11};
         System.out.println(Increasing(a));
     }
 
